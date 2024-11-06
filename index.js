1eb4b2ef443590f571226d3992f4f09763d0d79d
pietropn <78878397+pietropn@users.noreply.github.com>
2024-11-06 11:36:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-alert("Test");
-console.log(alert);
+let day;
+switch (new Date().getDay()) {
+    case 0:
+      day = "Domingo";
+      break;
+    case 1:
+      day = "Segunda";
+      break;
+    case 2:
+      day = "Terça";
+      break;
+    case 3:
+      day = "Quarta";
+      break;
+    case 4:
+      day = "Quinta";
+      break;
+    case 5:
+      day = "Sexta";
+      break;
+    case  6:
+      day = "Sábado";
+  }
+  document.getElementById("demo").innerHTML = day;
